6b3bedc43fa929a73f9c89492c74551bff085456
magalipujol <magalicpujol@gmail.com>
2021-10-22 08:35:11 -0300

    using cli

--- product-and-cart/src/main.js
+++ product-and-cart/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './assets/styles/style.scss'
+// * this is the same as:
+// const app = createApp(App)
+// app.use(router)
+// app.mount('#app')
 createApp(App).use(router).mount('#app')
